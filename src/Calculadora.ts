1063edd82ef9a83842732b8a17a7a5ed86c20cc2
fatia1 <130059555+MeErickss@users.noreply.github.com>
2024-09-27 08:59:01 -0300

    calculadora.ts

--- src/Calculadora.ts
+++ src/Calculadora.ts
@@ -1,17 +1,29 @@
-import Teclado from "./Teclado"
-import Tela from "./Tela"
-import Cpu from "./Cpu"
-
-export default class Calculadora {
-    definaTela(tela: Tela){
-
-    }
-
-    definaTeclado(teclado: Teclado){
-
-    }
-
-    definaCpu(cpu: Cpu){
-        
-    }
-}
+export enum Digito { ZERO = 0, UM, DOIS, TRÊS, QUATRO, CINCO, SEIS, SETE, OITO, NOVE }
+  
+  export enum Operação { SOMA, SUBTRAÇÃO, MULTIPLICAÇÃO, DIVISÃO, RAIZ_QUADRADA, PERCENTUAL }
+  
+  export enum Controle { DESATIVAÇÃO, ATIVAÇÃO_LIMPEZA_ERRO, MEMÓRIA_LEITURA_LIMPEZA, MEMÓRIA_SOMA, MEMÓRIA_SUBTRAÇÃO, SEPARADOR_DECIMAL }
+  
+  export interface Tela {
+    mostre(digito: Digito): void;
+    limpe(): void;
+  }
+  
+  export interface Teclado {
+    digite(digito: Digito): void;
+    digite(operação: Operação): void;
+    digite(controle: Controle): void;
+  }
+  
+  export interface Cpu {
+    receba(digito: Digito): void;
+    receba(operação: Operação): void;
+    receba(controle: Controle): void;
+    reinicie(): void;
+  }
+  
+  export interface Calculadora {
+    tela: Tela;
+    teclado: Teclado;
+    cpu: Cpu;
+  }
