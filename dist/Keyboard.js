d001a0e886099d7f7e83931df62745efa80b2c7e
stelinha <136658788+jstelinha@users.noreply.github.com>
2024-10-04 00:09:27 -0300

    AAAAAAAA
    AAAAAAAAAAAA

--- dist/Keyboard.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Keyboard {
-    ligar() { }
-    digitarnumero(numero) { }
-    somar() { }
-    exibirRestultado() { }
-}
-exports.default = Keyboard;
