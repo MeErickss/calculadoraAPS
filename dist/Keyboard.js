8db208e394a943e604d643db452f0a974ff8817c
fatia1 <130059555+MeErickss@users.noreply.github.com>
2024-10-04 00:18:57 -0300

    backup

--- dist/Keyboard.js
+++ dist/Keyboard.js
@@ -1,9 +1,1 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Keyboard {
-    ligar() { }
-    digitarnumero(numero) { }
-    somar() { }
-    exibirRestultado() { }
-}
-exports.default = Keyboard;
