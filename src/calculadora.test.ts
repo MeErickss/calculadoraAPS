59c72cbcdf45ea004a30b19d6054a4cf59647272
Lauberr <130068637+Lauberr@users.noreply.github.com>
2024-12-06 08:49:18 -0300

    inserindo testes professor(incompleto)

--- src/calculadora.test.ts
+++ src/calculadora.test.ts
@@ -8,30 +8,58 @@ describe('Testando minha calculadora',() =>{
     cpu.definaTela(tela)
 
     test ('Testar 123+456', ()=>{
-        console.log("= Testando 123 + 456 ===========================");
-    [Digito.UM, Digito.DOIS, Digito.TRÊS].forEach((element) => {
-      cpu.recebaDigito(element);
+      console.log("= Testando 123 + 456 ===========================");
+      [Digito.UM, Digito.DOIS, Digito.TRÊS].forEach((element) => {
+        cpu.recebaDigito(element);
+      });
+
+      cpu.recebaOperacao(Operação.SOMA);
+      [Digito.QUATRO, Digito.CINCO, Digito.SEIS].forEach((element) => {
+        cpu.recebaDigito(element);
+      });
+
+      cpu.recebaControle(Controle.IGUAL);
+      expect(tela.digitos).toBe("579")
+      expect(tela.sinal).toBe(Sinal.POSITIVO)
+      expect(tela.memoria).toBeFalsy()
+      expect(tela.error).toBeFalsy()
     });
-
-    cpu.recebaOperacao(Operação.SOMA);
-    [Digito.QUATRO, Digito.CINCO, Digito.SEIS].forEach((element) => {
-      cpu.recebaDigito(element);
-    });
-
-    cpu.recebaControle(Controle.IGUAL);
-        expect(tela.digitos).toBe("579")
-        expect(tela.sinal).toBe(Sinal.POSITIVO)
-        expect(tela.memoria).toBeFalsy()
-        expect(tela.error).toBeFalsy()
-
-    });
-
 
     test ('Testar 12+34+56', ()=>{
-        
+      console.log("= Testando 12 + 34 + 56 ===========================");
+      [Digito.UM, Digito.DOIS].forEach((element) => {
+        cpu.recebaDigito(element);
+      });
+      
+      cpu.recebaOperacao(Operação.SOMA);
+      [Digito.TRÊS, Digito.QUATRO].forEach((element) => {
+        cpu.recebaDigito(element);
+      });
+      
+      cpu.recebaOperacao(Operação.SOMA);
+      [Digito.CINCO, Digito.SEIS].forEach((element) => {
+        cpu.recebaDigito(element);
+      });
+      
+      cpu.recebaControle(Controle.IGUAL);
+      expect(tela.digitos).toBe("")
+      expect(tela.sinal).toBe(Sinal.POSITIVO)
+      expect(tela.memoria).toBeFalsy()
+      expect(tela.error).toBeFalsy()
     })
 
     test ('Testar 12/10', ()=>{
-        
+      console.log("= Testando 12 / 10  ===========================");
+      [Digito.UM, Digito.DOIS].forEach((element) => {
+        cpu.recebaDigito(lement);
+      });
+
+      cpu.recebaOperacao(Operação.DIVISÃO);
+      [Digito.UM, Digito.ZERO].forEach((element) => {
+        cpu.recebaDigito(element);
+      });
+      
+      cpu.recebaControle(Controle.IGUAL);
+      
     })
 })
